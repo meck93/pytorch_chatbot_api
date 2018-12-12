17c13796135c151587596a69383c99c9e6ebee54
Moritz Eck <moritz.eck@gmail.com>
2018-12-12 23:22:40 +0100

    simplified app

--- Dockerfile
+++ Dockerfile
@@ -2,13 +2,13 @@ FROM python:3.7-alpine
 
 WORKDIR /usr/src/app
 
-COPY env.txt ./
-RUN ls -a
+# COPY env.txt ./
+# RUN ls -a
 
-RUN apk --no-cache --update-cache add musl-dev linux-headers g++
-RUN pip install -r env.txt --no-cache-dir
-RUN pip install https://download.pytorch.org/whl/cpu/torch-1.0.0-cp37-cp37m-linux_x86_64.whl --no-cache-dir
-RUN pip install torchvision --no-cache-dir
+RUN apk --no-cache add --virtual .builddeps gcc gfortran musl-dev && pip install numpy && apk del .builddeps && rm -rf /root/.cache
+RUN pip install flask
+RUN pip install https://download.pytorch.org/whl/cpu/torch-1.0.0-cp37-cp37m-linux_x86_64.whl
+RUN pip install torchvision
 RUN ls -a
 
 COPY src .
---
+++ app.py
@@ -0,0 +1,43 @@
+from flask import Flask, jsonify, abort, make_response, request
+
+# webapp
+app = Flask(__name__, template_folder='./')
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
+
+
+@app.route('/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+
+    if len(task) == 0:
+        abort(404)
+
+    return jsonify({'task': task[0]})
+
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
