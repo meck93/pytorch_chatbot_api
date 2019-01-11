b044c3206ccc00846775674950d4a537e107bb33
Moritz Eck <moritz.eck@gmail.com>
2019-01-11 16:08:30 +0100

    removed unnecessary code from app.py

--- app.py
+++ app.py
@@ -7,35 +7,10 @@ app = Flask(__name__, template_folder='./')
 # instantiate pre-trained seq2seq model
 predictor = Deploy()
 
-tasks = [
-    {
-        'id': 1,
-        'title': u'Buy groceries',
-        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': u'Learn Python',
-        'description': u'Need to find a good Python tutorial on the web',
-        'done': False
-    }
-]
 
-
-@app.route('/tasks', methods=['GET'])
+@app.route('/test', methods=['GET'])
 def get_tasks():
-    return jsonify({'tasks': tasks})
-
-
-@app.route('/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    task = [task for task in tasks if task['id'] == task_id]
-
-    if len(task) == 0:
-        abort(404)
-
-    return jsonify({'task': task[0]})
+    return jsonify('message': 'server up and running!')
 
 
 @app.route('/prediction', methods=['POST', 'GET'])
@@ -46,7 +21,7 @@ def prediction():
 
 @app.errorhandler(404)
 def not_found(error):
-    return make_response(jsonify({'error': 'Not found'}), 404)
+    return make_response(jsonify({'error': 'not found'}), 404)
 
 
 if __name__ == '__main__':
